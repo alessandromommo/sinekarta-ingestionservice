8cea628d59de5c611894de272af6a2bf58ceddd6
alessandro.mommo@gmail.com <alessandro.mommo@gmail.com@b74e6c8b-866b-dd71-11bd-a6f535c31eae>
2015-03-03 18:30:27 +0000

    Naming refactoring of METS classes.

--- sinekarta-ingestionservice-commons/src/main/java/org/sinekarta/ingestionservice/mets/package-info.java
+++ sinekarta-ingestionservice-commons/src/main/java/org/sinekarta/ingestionservice/mets/package-info.java
@@ -2,8 +2,8 @@
 // Questo file è stato generato dall'architettura JavaTM per XML Binding (JAXB) Reference Implementation, v2.2.11 
 // Vedere <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a> 
 // Qualsiasi modifica a questo file andrà persa durante la ricompilazione dello schema di origine. 
-// Generato il: 2015.03.02 alle 05:23:20 PM CET 
+// Generato il: 2015.03.03 alle 07:24:26 PM CET 
 //
 
 @javax.xml.bind.annotation.XmlSchema(namespace = "http://www.loc.gov/METS/", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package org.sinekarta.ingestion.beans;
+package org.sinekarta.ingestionservice.mets;
